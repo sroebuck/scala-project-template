c3af4e9f4acd48c8477fe169ec177a5cea778e37
Stuart Roebuck <stuart.roebuck@proinnovate.com>
2011-01-17 09:51:07 +0000

    Added some standard logging libraries to the project template. Modified the project template to also include source and javadoc files to aid development. Added some .gitkeep files to force the 'empty' file structure into the repository.

--- project/build/TemplateProject.scala
+++ project/build/TemplateProject.scala
@@ -5,9 +5,16 @@
 import sbt._
 
 class TemplateProject(info: ProjectInfo) extends DefaultProject(info) with posterous.Publish {
+  // Note that in all the dependencies below the source and javadoc links are there in order to download the source
+  // and javadoc during development to enhance the IDE development environment.
+
+  // Logging...
+  val slf4s = "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.2" % "compile;provided->sources,javadoc"
+  val logback = "ch.qos.logback" % "logback-classic" % "0.9.27" % "compile;provided->sources,javadoc"
 
   // Testing...
-  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test;provided->sources,javadoc"
+  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test;provided->sources,javadoc"
 
   // postereous.Publish configuration...
   override def extension = ".md"
